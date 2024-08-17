8a5158fe06c7abda0da8e2ede5a614a5b1e0d932
김가영 <yeong.kmj@gmail.com>
2024-08-18 03:56:33 +0900

    feat: user read

---
+++ src/main/java/appJam/hackerton/appjam_27/domain/group/service/GroupService.java
@@ -0,0 +1,4 @@
+package appJam.hackerton.appjam_27.domain.group.service;
+
+public class GroupService {
+}
--- src/main/java/appJam/hackerton/appjam_27/domain/time/service/TimeService.java
+++ src/main/java/appJam/hackerton/appjam_27/domain/time/service/TimeService.java
@@ -1,17 +1,15 @@
 package appJam.hackerton.appjam_27.domain.time.service;
 
-import java.sql.Time;
-
 import appJam.hackerton.appjam_27.domain.time.dto.req.TimeReq;
 import appJam.hackerton.appjam_27.domain.time.entity.TimeEntity;
 import appJam.hackerton.appjam_27.domain.time.repository.TimeRepository;
-import appJam.hackerton.appjam_27.global.exception.custom.user.NotFoundUserException;
-import appJam.hackerton.appjam_27.global.response.Response;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
 import appJam.hackerton.appjam_27.domain.user.entity.UserEntity;
 import appJam.hackerton.appjam_27.domain.user.repository.UserRepository;
+import appJam.hackerton.appjam_27.global.exception.custom.user.NotFoundUserException;
+import appJam.hackerton.appjam_27.global.response.Response;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -33,5 +31,7 @@ public class TimeService {
 
         return Response.of(HttpStatus.OK, "성공");
     }
+
+
 
 }
